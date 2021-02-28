3f6f64950230b0209ebcc35f5fd383482fec6370
Mayank <mayankbucha01@gmail.com>
2021-02-28 20:41:16 +0530

    Added test cases

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,6 +7,7 @@ const {sendWelcomeEmail, sendCancelEmail} = require('../emails/account')
 
 const router = new express.Router()
 
+// Signup new user
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
@@ -31,6 +32,7 @@ const upload = multer({
     }
 })
 
+// Upload profile pic
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
     const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
     req.user.avatar = buffer
@@ -40,12 +42,14 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
     res.status(400).send({error: error.message})
 })
 
+// Delete profile pic
 router.delete('/users/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined
     await req.user.save()
     res.status(200).send()
 })
 
+// View profile pic
 router.get('/users/:id/avatar', async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
@@ -59,6 +63,7 @@ router.get('/users/:id/avatar', async (req, res) => {
     }
 })
 
+// Login user
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
@@ -69,6 +74,7 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+// Logout user
 router.post('/users/logout', auth, async (req, res) => {
     try {
         req.user.tokens = req.user.tokens.filter((token) => {
@@ -81,6 +87,7 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
+// Logout user from all devices
 router.post('/users/logoutAll', auth, async (req, res) => {
     try {
         req.user.tokens = []
@@ -91,10 +98,12 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     }
 })
 
+// Get the user profile
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
+// Update user profile
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'age', 'password']
@@ -114,6 +123,7 @@ router.patch('/users/me', auth, async (req, res) => {
     }
 })
 
+// Delete user
 router.delete('/users/me', auth,  async (req, res) => {
     try {
         await req.user.remove()
